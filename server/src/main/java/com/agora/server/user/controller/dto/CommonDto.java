bc4102fcfb994d3761596052f7c2d1dc76543bee
LSH <mylifeforzug123@gmail.com>
2023-01-16 15:15:53 +0900

    [feat]: modify commondto

--- server/src/main/java/com/agora/server/user/controller/dto/CommonDto.java
+++ server/src/main/java/com/agora/server/user/controller/dto/CommonDto.java
@@ -4,13 +4,13 @@ import lombok.Getter;
 
 @Getter
 public class CommonDto {
+    private String social_id;
     private String email;
-    private String nickname;
     private String phone;
 
-    public void createCommonDto(String email, String nickname, String phone){
+    public void createCommonDto(String social_id, String email,  String phone){
+        this.social_id = social_id;
         this.email = email;
-        this.nickname = nickname;
         this.phone = phone;
     }
 }
