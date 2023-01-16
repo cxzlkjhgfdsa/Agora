61e59d775c26ddd3af1ee345e5271361bc51061e
nowgnas <swlee0376@gmail.com>
2023-01-16 17:02:21 +0900

    [feat]: naver login controller 주석 추가

--- server/src/main/java/com/agora/server/user/controller/NaverAuthController.java
+++ server/src/main/java/com/agora/server/user/controller/NaverAuthController.java
@@ -1,6 +1,6 @@
 package com.agora.server.user.controller;
 
-import com.agora.server.common.ResponseDTO;
+import com.agora.server.common.dto.ResponseDTO;
 import com.agora.server.user.service.NaverAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,6 +15,9 @@ import java.io.IOException;
 public class NaverAuthController {
     private final NaverAuthService naverAuthService;
 
+    /**
+     * join 시 get 요청
+     */
     @GetMapping("join/oauth")
     public void join() {
         try {
@@ -24,6 +27,9 @@ public class NaverAuthController {
         }
     }
 
+    /**
+     * login 시 get 요청
+     */
     @GetMapping("login/oauth")
     public void login() {
         try {
@@ -33,10 +39,14 @@ public class NaverAuthController {
         }
     }
 
+    /**
+     * login redirect
+     * @param code naver가 주는 code
+     * TODO code를 가지고 accessToken을 얻기 위한 요청을 해야 함
+     */
     @GetMapping("auth/login")
     public ResponseEntity<ResponseDTO> login(@RequestParam String code) {
         ResponseDTO res = new ResponseDTO();
-
 
         return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
     }
