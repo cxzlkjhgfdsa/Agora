668ff0b8a349ca766a2d86f1bff070a3e4132dc2
indeokiya <indeokiya@snu.ac.kr>
2023-01-18 17:53:01 +0900

    [refactor] : recoil atom 수정

--- client/src/stores/atoms.js
+++ client/src/stores/atoms.js
@@ -1,9 +1,19 @@
 // Recoil로 관리될 Atom을 정의하고 생성합니다.
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+// 페이지를 새로고침하면 recoil state가 증발하는데, 
+// recoil-persist (npm install recoil-persist)를 사용하면 sessionStorage or localStorage(default)에 보관 가능합니다.
+// 서버에서 refresh-token을 관리하고 프론트에서는 access-token만 받아서 sessionStorage에 저장하는 방식으로 구현합니다. - 인덕
+export const persistSessionState = recoilPersist({
+  key: "persistSessionState",
+  storage: sessionStorage,
+})
 
 export const userInfoState = atom({
   key: "userInfoState",
   default: { isLoggedIn: false },
+  effects_UNSTABLE: [persistSessionState],
 });
 
 export const debateInfoState = atom({
