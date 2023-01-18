748570ac0467a2f1f3de74f864a4e8d424a9db9a
오윤식 <skyland310@naver.com>
2023-01-18 11:09:10 +0900

    [feat] googlelogin loginresponsedto 생성 전 api spec에임시로 맞춘 코드

--- server/src/main/java/com/agora/server/user/controller/GoogleAuthController.java
+++ server/src/main/java/com/agora/server/user/controller/GoogleAuthController.java
@@ -3,7 +3,6 @@ package com.agora.server.user.controller;
 import com.agora.server.common.dto.ResponseDTO;
 import com.agora.server.user.controller.dto.CommonDto;
 import com.agora.server.user.controller.dto.SocialType;
-import com.agora.server.user.controller.dto.google.GetGoogleOAuthRes;
 import com.agora.server.user.controller.dto.google.GoogleOAuthToken;
 import com.agora.server.user.domain.User;
 import com.agora.server.user.repository.GoogleUserRepository;
@@ -37,10 +36,29 @@ public class GoogleAuthController {
 
     @ResponseBody
     @GetMapping(value = "request/auth/login/google/callback")
-    public GetGoogleOAuthRes googleLoginCallback(
+    public ResponseEntity<ResponseDTO> googleLoginCallback(
             @RequestParam(name = "code") String code)throws IOException{
-        GetGoogleOAuthRes getGoogleOAuthRes=googleOAuthService.oAuthLogin(code);
-        return getGoogleOAuthRes;
+        // 토큰 받기
+        GoogleOAuthToken googleOAuthToken = googleOAuthService.getGoogleLoginOAuthToken(code);
+        // 유저정보 받기
+        CommonDto googleUser = googleOAuthService.getGoogleUserInfo(googleOAuthToken);
+
+        User joinedUser = userService.checkDuplicateUser(googleUser.getSocial_id(), SocialType.GOOGLE);
+
+        ResponseDTO res = new ResponseDTO();
+
+        // 가입이 안되어있는 유저 -> RuntimeException 발생 -> Exception 후에 커스텀으로 만들면 교체
+        if(joinedUser == null) {
+            throw new RuntimeException("가입되지 않은 회원입니다 회원가입 페이지로 이동합니다");
+        }
+
+        // 가입이 되어있는 유저 -> 정상코드와 ResponseLoginDto 반환
+        res.setState(true);
+        res.setBody(googleUser);
+        res.setMessage("ok");
+        res.setStatusCode(200);
+
+        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
     }
 
     @ResponseBody
@@ -49,7 +67,7 @@ public class GoogleAuthController {
             @RequestParam(name = "code") String code)throws IOException{
 
         // 토큰 받기
-        GoogleOAuthToken googleOAuthToken = googleOAuthService.getGoogleOAuthToken(code);
+        GoogleOAuthToken googleOAuthToken = googleOAuthService.getGoogleJoinOAuthToken(code);
         // 유저정보 받기
         CommonDto googleUser = googleOAuthService.getGoogleUserInfo(googleOAuthToken);
 
@@ -61,6 +79,7 @@ public class GoogleAuthController {
         if(dupUser != null) {
             throw new RuntimeException("이미 가입되어 있습니다 로그인 페이지로 이동합니다");
         }
+        
 
         // 가입되어 있지 않은 유저 -> 정상 코드와 CommonDto 반환
         res.setState(true);
