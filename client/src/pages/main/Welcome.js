8b6dae9040a62ed32fd22aeba941ed3607909e74
윤재휘 <yjh200523@naver.com>
2023-01-25 13:37:33 +0900

    [feat]: 상단바 스크롤 이벤트 부착

--- client/src/pages/main/Welcome.js
+++ client/src/pages/main/Welcome.js
@@ -2,6 +2,24 @@ function Welcome() {
   return (
     <div>
       <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
+      <h1>This is Welcome page.</h1>
     </div>
   )
 }
