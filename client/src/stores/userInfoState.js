15d8934bf8f47c73e0f7eae15bef6f0c252d33fe
indeokiya <indeokiya@snu.ac.kr>
2023-01-27 10:58:34 +0900

    [fix]: 서버와 변수명 동기화

---
+++ client/src/stores/userInfoState.js
@@ -0,0 +1,21 @@
+// import { atom } from "recoil";
+// import { recoilPersist } from "recoil-persist";
+
+// export const persistAtom = recoilPersist({
+//   key: "user-info",
+//   storage: sessionStorage,
+// })
+
+// export const userInfoState = atom({
+//     key: "userInfoState",
+//     default: { 
+//       isLoggedIn: false,
+//       accessToken: "",
+//       userId: "",
+//       userNickname: "",
+//       socialType: "",
+//       userPhoto: "",
+//     },
+//     effects_UNSTABLE: [persistAtom],
+//   });
+
