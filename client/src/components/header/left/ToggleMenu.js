69770d4f3f1dcbe3f9ff0321eac0e1ba79fee150
윤재휘 <yjh200523@naver.com>
2023-01-27 11:00:51 +0900

    [fix]: 토글 메뉴와 헤더가 겹치는 현상 해결 (Fake Header 생성)

--- client/src/components/header/left/ToggleMenu.js
+++ client/src/components/header/left/ToggleMenu.js
@@ -5,6 +5,24 @@ import styled from "styled-components";
 import SearchBar from "../center/SearchBar";
 import ToggleMenuItem from "./ToggleMenuItem";
 
+// 토글 메뉴 가리기 위한 상단바
+const StyledFakeHeader = styled.div`
+  // 높이 고정
+  height: 64px;
+
+  // 헤더 색상 설정, 배경색 그림자색
+  background-color: #FFFFFF;  // 배경색 : 흰색
+  box-shadow: 1px 1px 1px #DCDCDC;  // 박스 그림자 설정
+
+  z-index: -1;
+  
+  // 상단 고정
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+
 const StyledToggleMenu = styled.ul`
   background-color: #DFDFDF;
 
@@ -16,15 +34,15 @@ const StyledToggleMenu = styled.ul`
 
   // 위치 설정
   position: absolute;
-  top: ${({ show }) => show ? 64 : -500}px;
+  top: ${({ show, height }) => show ? 64 : 64 + height}px;
   left: 0;
   // right: 0;
-  z-index: -1;
+  z-index: -2;
   box-shadow: 1px 1px 1px #DCDCDC;  // 박스 그림자 설정
 
   list-style: none;
 
-  transition: 1s;
+  transition: 0.8s;
 `;
 
 const StyledToggleSearchBarItem = styled.li`
@@ -40,11 +58,14 @@ function ToggleMenu() {
   const show = useRecoilValue(showToggleMenuState);
 
   let items = [];
+  let topLocation = 0;
+  const ITEM_HEIGHT = 48;  // 리스트 아이템 높이 상수, top 위치 조정해주기 위함
   
   const [userInfo, setUserInfo] = useRecoilState(userInfoState);
 
   const curPath = useLocation().pathname;
   if (curPath.startsWith("/debate/list")) {
+    // 검색창 추가
     items.push(
       <StyledToggleSearchBarItem>
         <SearchBar />
@@ -89,10 +110,29 @@ function ToggleMenu() {
     );
   }
 
+  // 추가된 리스트 아이템들의 높이만큼 top 위치를 올려 토글 전엔 안 보이게 하기
+  if (items.length > 0) {
+    // 토론방 목록 페이지의 경우 검색창이 추가되어 높이를 다르게 설정해야 함
+    if (curPath.startsWith("/debate/list")) {
+      topLocation -= 56;  // 검색창 높이
+      topLocation -= (items.length - 1) * ITEM_HEIGHT;  // 검색창을 제외한 다른 아이템들의 높이 반영
+    } else {  // 검색창이 없는 페이지의 경우 모두 동일한 높이의 아이템이므로 그대로 반영
+      topLocation -= items.length * ITEM_HEIGHT;
+    }
+  }
+
   return (
-    <StyledToggleMenu show={show} height={items.length * 50}>
-      {items.map(item => item)}
-    </StyledToggleMenu>);
+    <>
+      {/* 토글 시 기존 헤더와 겹치는 현상을 방지하기 위해 가짜 헤더 삽입 */}
+      <StyledFakeHeader show={show} />
+
+      {/* 토글 메뉴 */}
+      <StyledToggleMenu show={show} height={topLocation}>
+        {/* 메뉴 아이템 추가 */}
+        {items.map(item => item)}
+      </StyledToggleMenu>
+    </>
+  );
 }
 
 export default ToggleMenu;
