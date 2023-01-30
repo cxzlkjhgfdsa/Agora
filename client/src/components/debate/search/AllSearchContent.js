4079a87742e587e771cbc104b491768b57746513
윤재휘 <yjh200523@naver.com>
2023-01-30 15:43:15 +0900

    [fix]: 토론방 링크 재설정

--- client/src/components/debate/search/AllSearchContent.js
+++ client/src/components/debate/search/AllSearchContent.js
@@ -71,7 +71,7 @@ const EtcInfo = styled.span`
 `;
 
 function AllSearchContent({ content }) {
-  const roomUrl = content.room_id ? "/debate/room" + content.room_id : "/no-page";
+  const roomUrl = content.room_id ? "/debate/room/" + content.room_id : "/no-page";
   const title = content.room_name ? content.room_name : "토론방 정보 로딩 실패";
   const creator = content.room_creater_name;
   const viewers = content.room_watch_cnt;
