26d2a195f74c4a0cf4a0d56be8537b891cf5b77c
nowgnas <swlee0376@gmail.com>
2023-02-01 12:31:18 +0900

    [fix]: amend dockerfile, add test controller

---
+++ server/src/main/java/com/agora/server/TestController.java
@@ -0,0 +1,17 @@
+package com.agora.server;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class TestController {
+    @GetMapping("hello")
+    public String hello() {
+        log.info("this is back server");
+        return "hello";
+    }
+}
