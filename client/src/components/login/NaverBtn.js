f1d09215c1523754ef37d8513452943f7e6e4545
nowgnas <swlee0376@gmail.com>
2023-02-02 12:11:47 +0900

    [fix]: amend request url

--- client/src/components/login/NaverBtn.js
+++ client/src/components/login/NaverBtn.js
@@ -1,7 +1,7 @@
 import naverLogo from "../../assets/login/btnG_완성형.png"
 
 function NaverBtn() {
-    const NAVER_AUTH_URL = `${process.env.REACT_APP_SERVER_BASE_URL}/oauth2/authorization/naver`
+    const NAVER_AUTH_URL = `${process.env.REACT_APP_SERVER_BASE_URL}/login/naver`
 
     return (
         <div>
