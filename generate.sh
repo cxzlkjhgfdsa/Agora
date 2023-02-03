79ec4c7879c696e6f2471b50e28ed652749f8523
nowgnas <marshmello.sw@gmail.com>
2023-02-03 16:00:40 +0900

    [chore]: amend shell script

--- generate.sh
+++ generate.sh
@@ -5,3 +5,4 @@ characters='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 version=$(cat /dev/urandom | tr -dc "$characters" | fold -w "$length" | head -n 1)
 echo "$version"
 export version='$verison'
+source ~/.bashrc
