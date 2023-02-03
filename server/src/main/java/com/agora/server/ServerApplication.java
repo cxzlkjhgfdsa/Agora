0e0adaf12ea4bf01a833cf8b63a3c4a5a0a8400f
nowgnas <marshmello.sw@gmail.com>
2023-02-03 12:49:29 +0900

    [chore]: amend docker file and aemend nginx

--- server/src/main/java/com/agora/server/ServerApplication.java
+++ server/src/main/java/com/agora/server/ServerApplication.java
@@ -1,14 +1,8 @@
 package com.agora.server;
 
-import com.agora.server.config.EnvConfig;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.PropertySource;
-
-import javax.persistence.EntityManager;
 
 
 @SpringBootApplication
