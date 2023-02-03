b3e9173ca740161cdc9678322c6bb21ee8de2bc7
nowgnas <marshmello.sw@gmail.com>
2023-02-03 15:58:10 +0900

    [chore]: add version env

--- generate.sh
+++ generate.sh
@@ -4,3 +4,4 @@ length=10
 characters='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 version=$(cat /dev/urandom | tr -dc "$characters" | fold -w "$length" | head -n 1)
 echo "$version"
+export version='$verison'
