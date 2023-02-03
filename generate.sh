54df0e485839e2ac3ed8b9096f9cafc7ea959e89
nowgnas <marshmello.sw@gmail.com>
2023-02-03 15:49:16 +0900

    [chore]: add generate random string

---
+++ generate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+length=10
+characters='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+version=$(cat /dev/urandom | tr -dc "$characters" | fold -w "$length" | head -n 1)
+echo "$version"
