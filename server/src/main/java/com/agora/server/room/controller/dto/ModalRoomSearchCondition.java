af532199fef7c38fb514c0a67d893144d7349775
오윤식 <skyland310@naver.com>
2023-02-03 09:17:52 +0900

    [refator] room dto fields change to CamelCase

--- server/src/main/java/com/agora/server/room/controller/dto/ModalRoomSearchCondition.java
+++ server/src/main/java/com/agora/server/room/controller/dto/ModalRoomSearchCondition.java
@@ -1,17 +1,13 @@
 package com.agora.server.room.controller.dto;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
 import lombok.Data;
-
-import javax.persistence.OrderBy;
-import java.util.List;
 
 @Data
 public class ModalRoomSearchCondition {
 
     // 토론진행여부, 카테고리, 정렬순서
 
-    private Boolean roomstate;
+    private Boolean roomState;
 
     private String category;
 
