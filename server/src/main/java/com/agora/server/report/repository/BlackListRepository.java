8047e2fed2263918582a041a5db5885912ef0097
LSH <mylifeforzug123@gmail.com>
2023-02-15 10:50:59 +0900

    [fix] : fix userJoin, edit method

--- server/src/main/java/com/agora/server/report/repository/BlackListRepository.java
+++ server/src/main/java/com/agora/server/report/repository/BlackListRepository.java
@@ -1,4 +1,12 @@
 package com.agora.server.report.repository;
 
-public interface BlackListRepository {
+import com.agora.server.report.domain.BlackList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface BlackListRepository extends JpaRepository<BlackList, Long> {
+
+    @Query("select b from BlackList b where b.user_phone=:user_phone")
+    BlackList findBlackListBydUser_phone(@Param("user_phone")String user_phone);
 }
--- server/src/main/java/com/agora/server/user/controller/UserController.java
+++ server/src/main/java/com/agora/server/user/controller/UserController.java
@@ -9,6 +9,8 @@ import com.agora.server.common.dto.ResponseDTO;
 import com.agora.server.encrypt.domain.Encrypt;
 import com.agora.server.encrypt.service.EncryptService;
 import com.agora.server.file.service.FileService;
+import com.agora.server.report.domain.BlackList;
+import com.agora.server.report.repository.BlackListRepository;
 import com.agora.server.user.controller.dto.request.EditRequestDto;
 import com.agora.server.user.controller.dto.request.LoginRequestDto;
 import com.agora.server.user.controller.dto.request.RequestCertificateByPhoneNumber;
@@ -39,17 +41,13 @@ public class UserController {
 
     private final UserService userService;
     private final UserRepository userRepository;
-
-    private final UserCategoryRepository userCategoryRepository;
     private final JwtTokenProvider tokenProvider;
-    private final EncryptService encryptService;
-
-    private final FileService fileService;
+    private final BlackListRepository blackListRepository;
 
     /**
      * RequestBody로 회원가입자의 정보를 받아옴
      * 회원가입을 위한 메소드
-     *
+     * 전화번호를 통해 블랙리스트를 조회, 블랙리스트에 등록되어있는 전화번호라면 회원가입 불가능
      * @param requestJoinDto
      * @return 회원가입이 정상적으로 실행되었다는 메세지를 보냄
      */
@@ -58,23 +56,19 @@ public class UserController {
     public ResponseEntity<ResponseDTO> userJoin(@RequestBody RequestJoinDto requestJoinDto) throws Exception {
         ResponseDTO responseDTO = new ResponseDTO();
 
-        Encrypt encrypt = Encrypt.createEncrypt(requestJoinDto.getUser_social_id());
-        // 카테고리 객체로 리스트 얻기
-        List<Category> categoryList = userService.findCategoryById(requestJoinDto.getCategories());
-        String encryptedUserName = encryptService.getEncryptedUserName(encrypt, requestJoinDto);
+        String user_phone = requestJoinDto.getUser_phone();
+        BlackList blackList= blackListRepository.findBlackListBydUser_phone(user_phone);
 
-        User joinUser = User.createUser(encrypt, requestJoinDto.getUser_social_type(), requestJoinDto.getUser_social_id()
-                , encryptedUserName, requestJoinDto.getUser_age(), requestJoinDto.getUser_phone(),
-                requestJoinDto.getUser_nickname(), requestJoinDto.getUser_photo_name(), requestJoinDto.getUser_photo());
-        User saveUser = userService.join(joinUser); // 1차캐시로 영속성 컨텍스트에 user를 올림
-        // 더티 체킹과 변경감지 !! 다시 공부하기
-
-        for (Category category : categoryList) {
-            UserCategory userCategory = userCategoryRepository.save(UserCategory.createUserCategory(saveUser, category));
-            saveUser.addCategories(userCategory);
+        System.out.println("여긴와요?");
+        if(blackList==null){ // 블랙리스트에 존재하지 않은 사용자
+            userService.userjoin(requestJoinDto);
+            responseDTO.setState(true);
+            responseDTO.setMessage("회원가입에 성공하셨습니다");
+        }else{  // 블랙리스트에 추가된 사용자
+            responseDTO.setState(false);
+            responseDTO.setMessage("악질 사용자는 회원가입 할 수 없습니다");
         }
-        responseDTO.setState(true);
-        responseDTO.setMessage("회원가입에 성공하셨습니다");
+
         return ResponseEntity.ok(responseDTO);
     }
 
