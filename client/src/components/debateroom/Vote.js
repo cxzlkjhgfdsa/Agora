b71c313c825231956a8de5ff7530081ecb8e0178
윤재휘 <yjh200523@naver.com>
2023-02-15 17:56:04 +0900

    [fix]: useRecoilState import 에러 수정

--- client/src/components/debateroom/Vote.js
+++ client/src/components/debateroom/Vote.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useRecoilState, useState, useEffect } from "react";
 import styled, { keyframes } from "styled-components";
 import customAxios from "utils/customAxios";
 
