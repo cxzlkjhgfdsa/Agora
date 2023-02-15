e1e8897176c3c541d9eab184963603272e10ffd8
nowgnas <swlee0376@gmail.com>
2023-02-15 10:46:08 +0900

    [docs]: 제출 자료 정리

---
+++ exec/database/insert.sql
@@ -0,0 +1,20 @@
+insert into category (category_name, category_image)
+values ('일상', null);
+insert into category (category_name, category_image)
+values ('음식', null);
+insert into category (category_name, category_image)
+values ('영화/드라마', null);
+insert into category (category_name, category_image)
+values ('연애', null);
+insert into category (category_name, category_image)
+values ('게임', null);
+insert into category (category_name, category_image)
+values ('IT/전자제품', null);
+insert into category (category_name, category_image)
+values ('스포츠', null);
+insert into category (category_name, category_image)
+values ('패션', null);
+insert into category (category_name, category_image)
+values ('공부', null);
+insert into category (category_name, category_image)
+values ('음악', null);
