e2836c68c6d5b0d38332579abfdf56527c45baeb
HwiHwi <yjh200523@naver.com>
2023-02-17 00:18:42 +0900

    [refactor]: Footer 추가로 인한 App 구조 리팩토링

--- client/src/App.js
+++ client/src/App.js
@@ -24,30 +24,32 @@ import TestMoadlPage from "pages/TestModalPage";
 function App() {
 
   return (
-    <>
-      <GlobalStyle />
-      <BrowserRouter>
-        <ScrollToTop />
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Welcome />} />
-            <Route path="/user/login/redirect-handler" element={<LoginRedirectHandler />} />
-            <Route path="/user/login" element={<Login />} />
-            <Route path="/user/signup/SNS" element={<Login />} />
-            <Route path="/user/signup/input" element={<SignUpInput />} />
-            <Route path="/user/signup/category" element={<SignUpCategory />} />
-            <Route path="/user/signup/complete" element={<SignUpComplete />} />
-            <Route path="/user/mypage" element={<MyPage />} />
-            <Route path="/debate/list" element={<DebateList />} />
-            <Route path="/debate/room/:roomId" element={<DebateRoom />} />
-            <Route path="/debate/search" element={<SearchRoom />} />
-            <Route path="/test-page" element={<TestMoadlPage />} />
-          </Routes>
-        </main>
+      <div style={{display: "flex", flexDirection: "column", height: "100vh"}}>
+        <GlobalStyle />
+        <div style={{flex: 1}}>
+          <BrowserRouter>
+            <ScrollToTop />
+            <Header />
+            <main>
+              <Routes>
+                <Route path="/" element={<Welcome />} />
+                <Route path="/user/login/redirect-handler" element={<LoginRedirectHandler />} />
+                <Route path="/user/login" element={<Login />} />
+                <Route path="/user/signup/SNS" element={<Login />} />
+                <Route path="/user/signup/input" element={<SignUpInput />} />
+                <Route path="/user/signup/category" element={<SignUpCategory />} />
+                <Route path="/user/signup/complete" element={<SignUpComplete />} />
+                <Route path="/user/mypage" element={<MyPage />} />
+                <Route path="/debate/list" element={<DebateList />} />
+                <Route path="/debate/room/:roomId" element={<DebateRoom />} />
+                <Route path="/debate/search" element={<SearchRoom />} />
+                <Route path="/test-page" element={<TestMoadlPage />} />
+              </Routes>
+            </main>
+          </BrowserRouter>
+        </div>
         <Footer />
-      </BrowserRouter>
-    </>
+      </div>
   );
 }
 
